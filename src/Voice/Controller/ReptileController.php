5a40735a935bc5a5ae6d3ea1696daac8cf136e7d
Administrator <sunkejl@live.com>
2014-06-20 13:30:17 +0800

    Module.config

---
+++ src/Voice/Controller/ReptileController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Voice\Controller;
+
+
+use Voice\Entity\PositionEntity;
+use Zend\Dom\Document\Query;
+use Zend\Dom\Document;
+use Zend\Http\ClientStatic;
+use Zend\Http\Response;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ReptileController extends AbstractActionController
+{
+
+
+
+    public function indexAction()
+    {
+
+        $content = file_get_contents('http://voice.hupu.com/nba/nba');
+
+
+        $content = mb_convert_encoding($content, 'HTML-ENTITIES', 'utf-8');
+
+        $document = new Document($content);
+        $nodeList = QUERY::execute('.whiteBorder dl', $document, Query::TYPE_CSS);
+
+//        var_dump(iconv('gbk','utf-8',$nodeList->current()->nodeValue));exit;
+
+        foreach ($nodeList as $node) {
+            /**
+             * @var $node \DOMElement
+             */
+            echo $node->nodeValue, '<br>';
+        }
+
+        exit;
+    }
+
+
+}
