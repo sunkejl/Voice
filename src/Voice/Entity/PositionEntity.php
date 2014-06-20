ac370e0c077a4651ca473b01f39db4d40f835bce
Administrator <sunkejl@live.com>
2014-06-20 13:31:32 +0800

    controller

---
+++ src/Voice/Entity/PositionEntity.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Voice\Entity;
+
+class PositionEntity
+{
+    protected $name;
+    protected $id;
+    protected $parent_id;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $parent_id
+     */
+    public function setParentId($parent_id)
+    {
+        $this->parent_id = $parent_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentId()
+    {
+        return $this->parent_id;
+    }
+
+
+}
